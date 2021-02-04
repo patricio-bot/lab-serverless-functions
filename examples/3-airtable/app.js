188df997118eaffd560a032e61dc9ef511f76d8b
Patricio Castro <luckydrawings@gmail.com>
2021-02-04 13:52:00 +0100

    airtable functions complete

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,25 @@
 const result = document.querySelector('.result')
+
+const fetchProducts = async () => {
+
+    try {
+        /*separate functions*/
+        //const { data } = await axios.get('/api/airtable')
+        /*working with one function complete*/
+        const { data } = await axios.get('/api/complete')
+        const products = data.map((product) => {
+            const { id, url, name, price } = product
+            return `<a href="product.html?id=${id}" class="product">
+            <img src="${url}" alt="${name}"/>
+            <div className="info">
+            <h5>${name}</h5>
+            <h5 className="price">$${price}</h5>
+            </div>
+            </a>`
+        }).join('')
+        result.innerHTML = products
+    } catch (error) {
+        result.innerHTML = '<h4>There was an error</h4>'
+    }
+}
+fetchProducts()
